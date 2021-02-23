b73baeb842e99532ce2a5633f6826d34bfaadf0e
Rodrigo Valla <rodrigovalla@protonmail.ch>
2021-02-23 16:21:54 -0300

    Working with misc...

---
+++ misc/trigger.py
@@ -0,0 +1,7 @@
+from composite import Composite
+
+comp = Composite("input/", "output/")
+
+comp.attractorMask((0,0,0), "mask_a_1.jpg", "test_8.jpg", "char_wave_3.jpg")
+
+comp.attractorMask((0,0,0), "mask_b_1.jpg", "test_9.jpg", "char_wave_4.jpg")
